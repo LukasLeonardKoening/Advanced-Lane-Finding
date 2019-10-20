c26c698a2e4453976abb02904b64258f87eca846
Lukas Leonard Köning <llkoening@posteo.de>
2019-10-20 20:08:11 +0200

    Added color threshold

--- test_Sobel.py
+++ test_Sobel.py
@@ -58,7 +58,7 @@ def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
     dir_binary[(direction > thresh[0]) & (direction < thresh[1])] = 1
     return dir_binary
 
-image = mpimg.imread('test_images/signs_vehicles_xygrad.png')
+image = mpimg.imread("test_images/test1.jpg")
 
 # Choose a Sobel kernel size
 ksize = 3 # Choose a larger odd number to smooth gradient measurements
@@ -69,8 +69,34 @@ grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(50, 100)
 mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(50, 100))
 dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1.3))
 
+# Image size
+xsize = image.shape[1]
+ysize = image.shape[0]
+
+# Thresholds
+lower_s_threshold = 150
+upper_s_threshold = 250
+lower_h_threshold = 22
+upper_h_threshold = 25
+
+# Color conversions
+hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+s_hls = hls[:,:,2]
+h_hls = hls[:,:,0]
+h_hls[:int(2*ysize/3),:] = 0 # Had to remove some environment
+
+# Color thresholds
+s_threshold = (s_hls > lower_s_threshold) & (s_hls <= upper_s_threshold)
+h_threshold = (h_hls > lower_h_threshold) & (h_hls <= upper_h_threshold)
+#color_based_threshold = (s_threshold | h_threshold)
+color_based_threshold = s_threshold
+
 combined = np.zeros_like(dir_binary)
-combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1)) | color_based_threshold] = 1
+#combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+#combined[((grady == 1) & (gradx == 1))] = 1
+#combined[((mag_binary == 1) & (dir_binary == 1))] = 1
+#combined[color_based_threshold] = 1
 
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
