c04391bc98e41c40b2ad508986ca75b64c80bba2
Lukas Leonard Köning <llkoening@posteo.de>
2019-10-24 20:12:36 +0200

    Added camera calibration function

--- helpers.py
+++ helpers.py
@@ -5,3 +5,40 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpl_img
 import glob
 import helpers
+
+def calibrateCamera():
+    """
+    Calculates parameters needed for camera calibration with the help of the calibration images
+    OUTPUT: Two arrays with object points (obj_points) and image points (img_points)
+    """
+    # Load images from folder camera_cal with the help of the glob library
+    images = glob.glob("camera_cal/calibration*.jpg")
+
+    # Arrays to save object~ / image points from all images
+    obj_points = []
+    img_points = []
+
+    # Preperation of objectpoints 
+    objp = np.zeros((6*9,3), np.float32)
+    for y in range(6):
+        for x in range(9):
+            objp[9*y+x] = [x,y,0]
+
+    # Loop over all calibration images
+    for image_path in images:
+        # Read in image
+        image = mpl_img.imread(image_path)
+
+        # Convert Image to gray scale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Find chessboard corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+        img = cv2.drawChessboardCorners(image, (9,6), corners, ret)
+
+        # Append image and object points  to the arrays
+        if ret:
+            img_points.append(corners)
+            obj_points.append(objp)
+
+    return obj_points, img_points
