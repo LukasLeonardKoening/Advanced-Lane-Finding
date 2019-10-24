f5c7cca06fb40d8208c65a9cdecfe9a1cd5cd1a1
Lukas Leonard Köning <llkoening@posteo.de>
2019-10-24 20:05:11 +0200

    Added variable declerations

--- Advanced_Lane_Finding.py
+++ Advanced_Lane_Finding.py
@@ -6,3 +6,7 @@ import matplotlib.image as mpl_img
 import glob
 import helpers
 
+# variable decleration
+test_image = plt.imread("test_images/test4.jpg")
+left_lane_line = Line()
+right_lane_line = Line()
