df784805574d7201e9875ff65feece03a782b2a4
Lukas Leonard Köning <llkoening@posteo.de>
2019-10-24 20:05:27 +0200

    Added Line() class

--- Advanced_Lane_Finding.py
+++ Advanced_Lane_Finding.py
@@ -10,3 +10,23 @@ import helpers
 test_image = plt.imread("test_images/test4.jpg")
 left_lane_line = Line()
 right_lane_line = Line()
+
+# line class decleration
+class Line():
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False  
+        # x values of the last n fits of the line
+        self.recent_xfitted = [] 
+        # last coefficients 
+        self.recent_coeff = []
+        #polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]  
+        #radius of curvature of the line in meters
+        self.radius_of_curvature = None 
+        #distance in meters of vehicle center from the line
+        self.line_base_pos = None 
+        #x values for detected line pixels
+        self.allx = None  
+        #y values for detected line pixels
+        self.ally = None
